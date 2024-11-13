3053c374f87a4510f533aabbe39156052eae87ba
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-11-13 10:26:17 -0800

    Installed eslint for react and typescript and extended configuration in config files

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,19 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended', 
+  ]},
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {parser: "@typescript-eslint/parser",
+    parserOptions: {
+    project: "./tsconfig.json",
+}},
 ];
