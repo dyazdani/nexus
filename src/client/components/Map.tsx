850dd3bcbbefaaca783a46ab42d8bb3e3d16f97e
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-11-20 18:31:35 -0800

    Changed name of Map.tsx to NexusMap.tsx, but 'Map' is not allowed for a component name

--- src/client/components/Map.tsx
+++
@@ -1,5 +0,0 @@
-const Map = () => {
-    return <div id="map"></div>;
-};
-
-export default Map;
---
+++ src/client/components/NexusMap.tsx
@@ -0,0 +1,12 @@
+const NexusMap = () => {
+    return (
+        <div
+            id="nexus-map"
+            style={{
+                height: '180px',
+            }}
+        ></div>
+    );
+};
+
+export default NexusMap;
