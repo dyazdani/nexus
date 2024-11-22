f46911921cce96667ae3970408a329ea3f5021d9
Dara Yazdani <dara.s.yazdani@gmail.com>
2024-11-21 23:47:15 -0800

    Markers for all of the origin coordinates of nexus now display on NexusMap

--- src/client/components/NexusMap.tsx
+++ src/client/components/NexusMap.tsx
@@ -4,13 +4,23 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import ChangeView from './ChangeView';
 
 interface NexusMapProps {
+    originPositions: Position[];
     nexusPosition: Position;
     nexusAddress: string;
 }
 
-const NexusMap = ({ nexusPosition, nexusAddress }: NexusMapProps) => {
+const NexusMap = ({
+    nexusPosition,
+    nexusAddress,
+    originPositions,
+}: NexusMapProps) => {
     // Create new array with Position returned by turf.js with type that Leaflet.js accepts
     const nexusLatLngTuple: LatLngTuple = [nexusPosition[1], nexusPosition[0]];
+    const originLatLngTuples: LatLngTuple[] = [
+        [originPositions[0][1], originPositions[0][0]],
+        [originPositions[1][1], originPositions[1][0]],
+        [originPositions[2][1], originPositions[2][0]],
+    ];
 
     return (
         <MapContainer
@@ -28,6 +38,15 @@ const NexusMap = ({ nexusPosition, nexusAddress }: NexusMapProps) => {
             />
             <Marker position={nexusLatLngTuple}>
                 <Popup>{nexusAddress}</Popup>
+            </Marker>
+            <Marker position={originLatLngTuples[0]}>
+                <Popup>First Coordinate</Popup>
+            </Marker>
+            <Marker position={originLatLngTuples[1]}>
+                <Popup>Second Coordinate</Popup>
+            </Marker>
+            <Marker position={originLatLngTuples[2]}>
+                <Popup>Third Coordinate</Popup>
             </Marker>
             <ChangeView position={nexusLatLngTuple} zoom={13} />
         </MapContainer>
